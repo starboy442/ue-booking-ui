0aa1eb46191622dda3dbd9e411df6a8b6e9ad8c0
starboy442 <zainulhaq@hitostudios.net>
2024-07-10 18:39:32 +0500

    Remove Consoles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import AppRouter from "./navigation/AppRouter";
-import { AuthContext } from "./api/Auth";
-import { useContext } from "react";
 export default function App() {
-  const { loader } = useContext(AuthContext);
-  console.log(loader);
   return <AppRouter />;
 }
